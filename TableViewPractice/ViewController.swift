9ba7aa8c24a603dfb5b845841868a06c44b838cc
Akib Shahjahan <AkibShahjahan@v1020-wn-249-0.campus-dynamic.uwaterloo.ca>
2016-01-07 18:14:19 -0500

    Initial Commit

---
+++ TableViewPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by Akib Shahjahan on 2016-01-07.
+//  Copyright © 2016 Akib Shahjahan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
